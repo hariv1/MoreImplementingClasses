267703e7a3e2c73d8072c7abde852b37dbf10b71
hariv1 <hariv@rose-hulman.edu>
2017-12-23 10:18:20 -0500

    DONE

--- src/m1_Line.py
+++ src/m1_Line.py
@@ -405,7 +405,7 @@ class Line(object):
           :rtype: float
         """
         # --------------------------------------------------------------
-        # TODO: 6.
+        # DONE: 6.
         #   a. READ the above specification, including the Example.
         #        ** ASK QUESTIONS AS NEEDED. **
         #        ** Be sure you understand it, ESPECIALLY the Example.
@@ -413,6 +413,13 @@ class Line(object):
         #        The tests are already written (below).
         #        They include the Example in the above doc-string.
         # --------------------------------------------------------------
+        rise = self.end.y - self.start.y
+        run = self.end.x - self.start.x
+
+        if run == 0:
+            return math.inf
+
+        return (rise/run)
 
     def length(self):
         """
