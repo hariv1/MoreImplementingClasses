9b833a1a87c40b90d6b1216c33751fafc6638755
hariv1 <hariv@rose-hulman.edu>
2017-12-25 15:02:26 -0500

    DONE

--- src/m2_baby_class.py
+++ src/m2_baby_class.py
@@ -93,7 +93,26 @@ def main():
 class Baby(object):
 
     def __init__(self, name):
+        self.name = name
+        self.hour_count = 0
+        print("Hello baby "+ self.name + "!")
 
+    def feed_baby(self):
+        print("Thank you for feeding baby "+ self.name+".")
+        self.hour_count = 0
+
+    def hour_passes(self):
+
+        if self.hour_count == 0:
+            print("Baby "+ self.name + " is sleeping.")
+            self.hour_count = self.hour_count + 1
+        elif self.hour_count == 1:
+            print("Baby "+ self.name + " is awake. Time for food.")
+            self.hour_count = self.hour_count + 1
+        else:
+            print("Baby "+ self.name + " is CRYING uncontrollably! Feed the "
+                                       "Baby!")
+            self.hour_count = self.hour_count + 1
 
 
 
